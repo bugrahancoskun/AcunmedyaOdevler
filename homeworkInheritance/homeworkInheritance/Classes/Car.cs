b1c0dd0a8060bd4baf1eb0a7a5bf0509c88d4cdf
bugrahancoskun <bugrahan_coskun@hotmail.com>
2025-03-03 15:07:03 +0300

    homework
    inheritance konusu ödevlerle pekiştirildi.

---
+++ homeworkInheritance/homeworkInheritance/Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkInheritance.Classes
+{
+    public class Car
+    {
+        public string Marka;
+        public string VitesTuru;
+
+        public void car(string marka, string vitesTuru)
+        {
+            Marka = marka;
+            VitesTuru = vitesTuru;
+            Console.WriteLine("Aracın markası: " + Marka + " Aracın vites türü: " + VitesTuru);
+        }
+    }
+}
---
+++ homeworkInheritance/homeworkInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using homeworkInheritance.Classes;
+
+BMW bmw = new BMW();
+bmw.car("BMW", "Düz");
+
+Porsche porsche = new Porsche();
+porsche.car("Porsche", "Otomatik");
+
+Audi audi = new Audi();
+audi.car("Audi", "Otomatik");
+
+Mercedes mercedes = new Mercedes();
+mercedes.car("Mercedes", "Düz");
+
+Togg togg = new Togg();
+togg.car("Togg", "Otomatik");
+
+Toyota toyota = new Toyota();
+toyota.car("Toyota", "Düz");
