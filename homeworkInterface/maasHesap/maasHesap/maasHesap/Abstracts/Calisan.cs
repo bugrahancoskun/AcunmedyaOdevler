076c61d9b1868f281f9064497df024778dfa2ff4
bugrahancoskun <bugrahan_coskun@hotmail.com>
2025-03-07 18:40:59 +0300

    homework2

---
+++ homeworkInterface/maasHesap/maasHesap/maasHesap/Abstracts/Calisan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maasHesap.Abstracts
+{   
+    //1.abstract classların nesnesi oluşturulamaz.
+    //2.kendisinden türeyen classları eğer abstract class içinde normal metodların dışında
+    //abstract keyword ile tanımlanmış bir method varsa o methodu yazmak zorundadırlar
+    public abstract class Calisan
+    {
+        public abstract double maasinizNedir();
+        public void mesaiyeBasla(string isim)
+        {
+            Console.WriteLine(isim + " mesaiye başladı..");
+        }
+    }
+}
---
+++ homeworkInterface/maasHesap/maasHesap/maasHesap/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using maasHesap.Abstracts;
+GenelMudur gm = new GenelMudur();
+Mudur mu = new Mudur();
+Programcı pr = new Programcı();
+Stajyer s = new Stajyer();
+
+double toplamMaas = 0.0;
+toplamMaas += gm.maasinizNedir();
+toplamMaas += mu.maasinizNedir();
+toplamMaas += pr.maasinizNedir();
+toplamMaas += s.maasinizNedir();
+
+Console.WriteLine("Çalışanların Toplam Maaşı: " + toplamMaas);
