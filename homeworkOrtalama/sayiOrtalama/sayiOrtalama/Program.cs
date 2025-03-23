4aa5cf3d8f26ee1067d4d893cabfd0ca5960a5a2
bugrahancoskun <bugrahan_coskun@hotmail.com>
2025-03-23 22:17:32 +0300

    homeworkOrtalama

---
+++ homeworkOrtalama/sayiOrtalama/sayiOrtalama/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        int[] sayilar = new int[5];
+        int toplam = 0;
+
+        Console.WriteLine("Lütfen 5 adet sayı girin:");
+
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write((i + 1) + ". sayıyı girin: ");
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        foreach (int sayi in sayilar)
+        {
+            toplam += sayi;
+        }
+
+        
+        double ortalama = (double)toplam / 5;
+
+        
+        Console.WriteLine("Girdiğiniz sayıların ortalaması: " + ortalama);
+    }
+}
