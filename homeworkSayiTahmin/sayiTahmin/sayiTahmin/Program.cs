f849cead354d646af258909fd735cbe657f5ce1d
bugrahancoskun <bugrahan_coskun@hotmail.com>
2025-03-23 22:11:50 +0300

    homeworkSayiTahmin

---
+++ homeworkSayiTahmin/sayiTahmin/sayiTahmin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Random random = new Random();
+        int tutulanSayi = random.Next(1, 101);
+        int tahminSayisi = 0;
+        int tahmin;
+
+        Console.WriteLine("1 ile 100 arasında bir sayı tuttum. Tahmin etmeye çalış!");
+
+        do
+        {
+            
+            Console.Write("Tahmininiz: ");
+            tahmin = Convert.ToInt32(Console.ReadLine());
+            tahminSayisi++;
+
+            
+            if (tahmin < tutulanSayi)
+            {
+                Console.WriteLine("Daha büyük bir sayı girin.");
+            }
+            else if (tahmin > tutulanSayi)
+            {
+                Console.WriteLine("Daha küçük bir sayı girin.");
+            }
+            else
+            {
+                Console.WriteLine("Tebrikler! Doğru tahmin.");
+                Console.WriteLine("Toplam tahmin sayısı: " + tahminSayisi);
+            }
+        } while (tahmin != tutulanSayi);
+    }
+}
