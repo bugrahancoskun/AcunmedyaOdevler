59a65039c939369109ecf402b80fe61a7047c50b
bugrahancoskun <bugrahan_coskun@hotmail.com>
2025-03-23 21:43:28 +0300

    homeworkAccessModifers

---
+++ homeworkAccessmodifiers/accessModifiers/accessModifiers/Classes/Kullanici.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accessModifiers.Classes
+{
+    
+        public class Kullanici
+        {
+            
+            private string adSoyad;
+            private int yas;
+
+            
+            public string Email { get; set; }
+
+            
+            public string AdSoyad
+            {
+                get { return adSoyad; }
+                set { adSoyad = value; }
+            }
+
+           
+            public int Yas
+            {
+                get { return yas; }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Yaş negatif olamaz!");
+                    }
+                    yas = value;
+                }
+            }
+
+            public void BilgileriGoster()
+            {
+                Console.WriteLine("Ad Soyad: " + adSoyad);
+                Console.WriteLine("Yaş: " + yas);
+                Console.WriteLine("E-posta: " + Email);
+            }
+        }
+
+    }
---
+++ homeworkAccessmodifiers/accessModifiers/accessModifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿using accessModifiers.Classes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Kullanici kullanici = new Kullanici();
+
+       
+        kullanici.AdSoyad = "Bugrahan Coskun";
+        kullanici.Yas = 25;
+        kullanici.Email = "bugrahancoskun@gmail.com";
+
+        
+        kullanici.BilgileriGoster();
+
+        
+        try
+        {
+            kullanici.Yas = -5;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
